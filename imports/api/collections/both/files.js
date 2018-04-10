5944993e8d6ba2d38f3fd3ef007f4162661a0483
Mukul Hase <mukulhase@gmail.com>
2018-04-10 18:31:43 +0530

    AWS S3 bucket integration

--- imports/api/collections/both/files.js
+++ imports/api/collections/both/files.js
@@ -2,7 +2,18 @@ import {Mongo} from "meteor/mongo";
 import {Users} from "meteor-user-roles";
 
 export const Files = new FS.Collection("files", {
-	stores: [new FS.Store.S3("files", {})]
+	stores: [new FS.Store.S3("files", {
+        region: "ap-southeast-1", //optional in most cases
+        accessKeyId: "AKIAI2DUU4V5YVBSHXSA", //required if environment variables are not set
+        secretAccessKey: "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", //required if environment variables are not set
+        bucket: "scholargate", //required
+        ACL: "public-read-write", //optional, default is 'private', but you can allow public or secure access routed through your app URL
+        folder: "uploads", //optional, which folder (key prefix) in the bucket to use
+        // The rest are generic store options supported by all storage adapters
+        // transformWrite: myTransformWriteFunction, //optional
+        // transformRead: myTransformReadFunction, //optional
+        maxTries: 1 //optional, default 5
+    })]
 });
 
 Files.userCanInsert = function(userId, doc) {
