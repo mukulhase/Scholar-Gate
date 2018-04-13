94d4cf8857f7f1ba0409129f6554bac103f7d30c
blaze225 <saad.khan@students.iiit.ac.in>
2018-04-14 00:25:21 +0530

    search route

--- flask_server/search.py
+++ flask_server/search.py
@@ -7,3 +7,24 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 	return 'Hello, World!'
+
+@app.route('/search', methods=['GET','POST'])
+def search():
+	print (request.is_json)
+	content = request.get_json()
+	print(content)
+	schema = create_schema()
+	
+	if not os.path.exists("indexdir"):
+		os.mkdir("indexdir")
+		static_index()
+
+	ix = index.create_in("indexdir", schema)
+	add_pdf(content['docid'], content['title'], content['abstract'])
+	query = content['query']
+	print(query)
+	result = search(query,"title","abstract")
+	if result is None:
+		return json.dumps({"result":"No match found"})
+	else:
+		return json.dumps({"result":result})
