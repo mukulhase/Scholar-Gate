79aeea0b747334ff0f15ba798578cdb001d4e816
Divanshu Jain <96.djain@gmail.com>
2018-04-17 19:10:46 +0530

    Added functionality in Publication List (with button)

--- imports/ui/components/publication_list/publication_list.jsx
+++ imports/ui/components/publication_list/publication_list.jsx
@@ -144,3 +144,140 @@ export class PublicationList extends Component {
   }
 }
 
+export class PublicationSingle extends Component {
+    constructor() {
+        super();
+
+    }
+
+    componentWillMount() {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    componentDidMount() {
+        x = '.panel-success, .panel-info, .panel-warning, or .panel-danger';
+
+        Meteor.defer(function() {
+            globalOnRendered();
+        });
+    }
+
+
+    onClick(id) {
+        FlowRouter.go('publication', objectUtils.mergeObjects(FlowRouter.current().params, { publicationId: id }));
+    }
+
+    render() {
+        return (
+            <Panel defaultExpanded={false} key={this.props.publication._id}>
+              <Panel.Heading>
+                <Panel.Title onClick={() => {
+                    this.onClick(this.props.publication._id);
+                }}>{this.props.publication.title}</Panel.Title>
+                By {this.props.data.authors_list.map((obj) => <Label>{obj.name}</Label>)}
+                <br/>
+                <Panel.Toggle componentClass="a">Read More</Panel.Toggle>
+              </Panel.Heading>
+              <Panel.Collapse>
+                <Panel.Body>
+                  Abstract not parsed
+                </Panel.Body>
+              </Panel.Collapse>
+              <Panel.Footer>
+                  {
+                    <a className={'btn'} href={linkhelper(this.props.publication)}>Download</a>
+                  }
+                  {<this className="props children"></this>}
+              </Panel.Footer>
+            </Panel>
+        );
+    }
+}
+
+export const PublicationSingleContainer = withTracker(function(props) {
+    var isReady = function() {
+
+
+        var subs = [
+            Meteor.subscribe('authors_list')
+            // Meteor.subscribe("reports_list"),
+        ];
+        var ready = true;
+        _.each(subs, function(sub) {
+            if (!sub.ready())
+                ready = false;
+        });
+        return ready;
+    };
+
+    var data = {
+        dataLoading: true,
+        authors_list: []
+    };
+
+    if (isReady()) {
+        if(!props.publication.authorsids || props.publication.authorsids.length===0){
+            data = {authors_list:[]}
+        }else{
+            data = {
+                authors_list: Authors.find({
+                    _id: {
+                        $in: props.publication.authorsids
+                    }
+                }, {}).fetch()
+                // reports_pos: Reports.find({})
+            };
+        }
+
+
+    }
+    return { data: data };
+})(PublicationSingle);
+
+export class PublicationListWithClaim extends Component {
+    constructor() {
+        super();
+
+    }
+
+    componentWillMount() {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    componentDidMount() {
+        x = '.panel-success, .panel-info, .panel-warning, or .panel-danger';
+
+        Meteor.defer(function() {
+            globalOnRendered();
+        });
+    }
+
+
+    onClick(id) {
+        FlowRouter.go('publication', objectUtils.mergeObjects(FlowRouter.current().params, { publicationId: id }));
+    }
+
+    render() {
+        return (
+            <div className={'well well-sm'}>
+                {
+                    (this.props.publications && this.props.publications.length > 0) ? this.props.publications.map((obj) => (
+                        <PublicationSingleContainer publication={obj} key={obj._id}>
+                          <Button bsStyle={'success'} onClick={()=>{this.claim()}}> Claim </Button>
+                        </PublicationSingleContainer>
+                    )) : 'No Publications'
+                }
+
+            </div>
+        );
+    }
+}
+
